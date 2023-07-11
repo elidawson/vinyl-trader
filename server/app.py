8c5ad1dda7c710bfd9e5e483aa878235dfddb9c4
Eli Dawson <edawson@alumni.berklee.edu>
2023-07-10 21:18:11 -0400

    fix user serialize rules, create user get route

--- server/app.py
+++ server/app.py
@@ -1,12 +1,18 @@
-from models import Favorite, Comment, User, Record
-from flask import request, make_response
-from flask_restful import Api, Resource
-from config import app, db, api, Resource, migrate
 from models import *
+from flask import request, make_response
+from config import app, db, api, Resource, migrate
 
 @app.route('/')
 def hello():
     return 'hello from flask!'
+
+class Users( Resource ):
+    def get( self ):
+        users = [ user.to_dict() for user in User.query.all() ]
+        res = make_response(users, 200)
+        return res
+
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
