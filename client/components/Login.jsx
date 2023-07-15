b2c1851e08f6ae42e3a161704b6443d1227abac8
Eli Dawson <edawson@alumni.berklee.edu>
2023-07-14 20:10:45 -0400

    add basic login functionality

---
+++ client/components/Login.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+function Login({ setUser }) {
+    const navigate = useNavigate();
+
+    const formSchema = yup.object({
+        username: yup.string(),
+        password: yup.string(),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validationSchema: formSchema,
+        onSubmit: (values, actions) => {
+            fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            body: JSON.stringify(values),
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                actions.resetForm();
+                setUser(data);
+                navigate('/');
+            })
+            .catch(error => alert(error))
+        },
+    });
+    return (
+        <form className='form' onSubmit={formik.handleSubmit}>
+            <h1>login</h1>
+            <label>username</label>
+            <input value={formik.values.username} onChange={formik.handleChange} type='text' name='username' /><br/>
+            <label>password</label>
+            <input value={formik.values.password} onChange={formik.handleChange} type='password' name='password' /><br/>
+            <input type='submit' value='login' className='button' />
+        </form>
+    )
+}
+
+export default Login
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5555",
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\//, ""),
+      },
+    }
+  },
   plugins: [react()],
 })
